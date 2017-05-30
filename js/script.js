3f0c33c3b7a146bf5c6c3b47154f4ddf4039d157
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-05-30 12:26:19 +0300

    added a calculator

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var add=function(number1,number2){
+var number1=parseInt(prompt("Enter a number: "));
+var number2=parseInt(prompt("Enter another number1: "));
+
+return number1 + number2;
+
+};
+alert(add());
+var subtract=function(number3, number4){
+var number3=parseInt(prompt("Enter another number2: "));
+var number4=parseInt(prompt("Enter another number3: "));
+return number3 - number4;
+};
+alert(subtract());
+var multiply=function(number5,number6){
+var number5=parseInt(prompt("Enter another number4: "));
+var number6=parseInt(prompt("Enter another number5: "));
+return number5*number6;
+};
+alert(multiply());
+var divide=function(number7, number8){
+var number7=parseInt(prompt("Enter another number6: "));
+var number8=parseInt(prompt("Enter another number7: "));
+return number7, number8;
+};
+alert(divide());
